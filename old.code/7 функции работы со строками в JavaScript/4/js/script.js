a9c554c6104cccdcc31593916e961ee03288ed62
Andrii_M <https://www.google.com/webhp?authuser=2>
2021-04-20 14:31:53 +0300

    Add new theme - функции работы со строками в JavaScript 1/4

---
+++ old.code/7 функции работы со строками в JavaScript/4/js/script.js
@@ -0,0 +1,8 @@
+let str = 'я учу javascript!';
+let str = 'я учу javascript!';
+let substr = str.substr(0, 2);
+let substring = str.substring(0, 2);;
+let slice = str.slice(0, 2);
+console.log(substr);
+console.log(substring);
+console.log(slice);
